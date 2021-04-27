740c64547f404fbf5b57d918f1d5fb4ce7e54d65
jacobrobertsbaca <jacobrobertsbaca@gmail.com>
2021-04-27 07:18:51 -0600

    Make localization.js not run in local browser

--- localization/template/localization.js
+++ localization/template/localization.js
@@ -1,10 +1,14 @@
 (function() {
+    // Do not run in local mode
+    if (location.hostname === "localhost" || location.hostname === "127.0.0.1" || location.hostname === "")
+        return;
+
     var langs = {{LANG}};
 
     var scriptTag = document.currentScript;
     var page = scriptTag.getAttribute("page");
     var language = navigator.language.split('-')[0];
-    
+
     if (langs.hasOwnProperty(language)) {
         // The desired language is supported, redirect to it
         var redirect = langs[language] + page;
